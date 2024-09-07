cf532bd94a186a774037fecf35d5c97971553fa5
arunprasad712 <135154558+arunprasad712@users.noreply.github.com>
2024-09-07 18:10:26 +0530

    TodoList.js

--- TodoList.js
+++ TodoList.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 function TodoList(props) {
-  const { todoList } = props;
+  const { todoList, onComplete,onDeleteList} = props;
 
-  
   if (!Array.isArray(todoList)) {
     console.error('Expected todoList to be an array, but received:', todoList);
     return <div className="alert alert-danger">Error: todoList is not an array.</div>;
@@ -15,9 +14,17 @@ function TodoList(props) {
       <div className="list-group">
         {todoList.length > 0 ? (
           todoList.map((todo, index) => (
-            <h1 key={index} className="list-group-item list-group-item-action text-primary">
-              {todo}
-            </h1>
+            <div key={index} className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+              <h1 className="text-primary">{todo}</h1>
+              <div>
+              <button onClick={() => onDeleteList(index)} className="btn btn-danger btn-sm">
+                Delete
+              </button>
+              <button onClick={() => onComplete(index)} className="btn btn-danger btn-sm">
+                Completed
+              </button>
+              </div>
+            </div>
           ))
         ) : (
           <p className="alert alert-info">No tasks to show</p>
